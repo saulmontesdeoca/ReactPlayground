5ea0925d8d2e762669df16edfe41dd37f225b1bb
Saul Montes De Oca <mr.sauce97@gmail.com>
2019-06-19 18:52:18 -0500

    More cleaning

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const element = <h1>Hello Badge!</h1>;
+const container = document.getElementsById('root');
+
+ReactDOM.render(element, container);
