07059737e41920bfe4edfb8b72ebefa66ce9d0b5
Saul Montes De Oca <mr.sauce97@gmail.com>
2019-06-19 18:53:09 -0500

    Twas just a bug

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const element = <h1>Hello Badge!</h1>;
-const container = document.getElementsById('root');
+const container = document.getElementById('root');
 
 ReactDOM.render(element, container);
