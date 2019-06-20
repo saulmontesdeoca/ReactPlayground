922f24188a0f4f4f3066c5cea84583723870a04d
Saul Montes De Oca <mr.sauce97@gmail.com>
2019-06-20 16:48:41 -0500

    Creating the Badge component

---
+++ src/components/Badge.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Badge extends React.Component {
+    render(){
+        return (
+            <div>
+                <div>
+                    <img src="" alt="imagen de portada"/>
+                </div>
+                <div>
+                    <img src="" alt="Foto de perfil"/>
+                    <h1>Saul <br/>Montes De Oca</h1>
+                </div>
+                <div>
+                    <p>Software Engineer</p>
+                    <p>@saul_mdo</p>
+                </div>
+                <div>
+                    <p>#CampusParty</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Badge;
