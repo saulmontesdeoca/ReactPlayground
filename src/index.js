7e2d87c97b394d39687c80ca0e5f8980fd6f6f45
Saul Montes De Oca <mr.sauce97@gmail.com>
2019-06-20 16:37:08 -0500

    Fixing the container bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Badge from './components/Badge'
 
 const element = <h1>Hello Badge!</h1>;
 const container = document.getElementById('root');
 
-ReactDOM.render(element, container);
+ReactDOM.render(<Badge />, container);
