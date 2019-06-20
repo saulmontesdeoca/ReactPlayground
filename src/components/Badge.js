3a557cedbb19c0f507e9652c346f9a90f1179fc8
Saul Montes De Oca <mr.sauce97@gmail.com>
2019-06-20 17:40:50 -0500

    Changes to the badge and adding Bootstrap

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import confLogo from '../images/badge-header.svg'
+import confLogo from '../images/badge-header.svg';
+import '../pages/styles/Badges.css';
 
 class Badge extends React.Component {
     render(){
         return (
-            <div>
-                <div>
+            <div className="Badge">
+                <div className="Badge__header">
                     <img src={confLogo} alt="imagen de portada"/>
                 </div>
-                <div>
-                    <img src="https://www.gravatar.com/avatar?d=identicon" alt="Foto de perfil"/>
+                <div className="Badge__section-name">
+                    <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="Foto de perfil"/>
                     <h1>Saul <br/>Montes De Oca</h1>
                 </div>
-                <div>
+                <div className="Badge__section-info">
                     <p>Software Engineer</p>
                     <p>@saul_mdo</p>
                 </div>
-                <div>
+                <div className="Badge__footer">
                     <p>#CampusParty</p>
                 </div>
             </div>
