a80cb184891bae5bfc3d5da26fd4201d851a5055
Saul Montes De Oca <mr.sauce97@gmail.com>
2019-06-21 11:43:15 -0500

    Adding CSS and Bootstrap styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Badge from './components/Badge'
+import Badge from './components/Badge';
+import 'bootstrap/dist/css/bootstrap.css';
+import './global.css';
+
 
 const element = <h1>Hello Badge!</h1>;
 const container = document.getElementById('root');
