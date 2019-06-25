3053a003adaac561ec6daefd9ef7805ecac18116
Saul Montes De Oca <mr.sauce97@gmail.com>
2019-06-24 23:15:03 -0500

    Updating BadgeNew comp

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -2,6 +2,7 @@ import React from 'react';
 import header from '../images/badge-header.svg';
 import Navbar from '../components/Navbar';
 import Badge from '../components/Badge';
+import BadgeForm from '../components/BadgeForm';
 
 class BadgeNew extends React.Component {
     render(){
@@ -13,7 +14,7 @@ class BadgeNew extends React.Component {
                 </div>
                 <div className='container'>
                     <div className='row'>
-                        <div className='col'>
+                        <div className='col-6'>
                             <Badge 
                             firstName='Saul'
                             lastName='Montes De Oca'
@@ -22,6 +23,9 @@ class BadgeNew extends React.Component {
                             hashtag='ReactRocks'
                             avatarUrl='http://bothsides.jimihendrix.com/wp-content/uploads/2017/12/COVER_BothSidesOfTheSky.jpg'
                             />
+                        </div>
+                        <div className='col-6'>
+                            <BadgeForm />
                         </div>
                     </div>
                 </div>
